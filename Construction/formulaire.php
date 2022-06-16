59ab4b72d63c1494bdb83e7dda4c909caf177f4f
Mederick-ROBERT <mederick.robert@free.fr>
2022-06-16 20:09:55 +0200

    update construction ( test pour Nbr )

--- Construction/formulaire.php
+++ Construction/formulaire.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Titre en attente...</title>
-        <link rel="stylesheet" href="formulaire.css">
+        <link rel="stylesheet" href="formulaire.css" media="screen">
 
 </head>
 <body>
@@ -18,10 +18,29 @@
 
 <form action="insert.php" method="post">
 <br>
-<p>Nom:</p> <input type="text" name="Nom" class="barre" />
-<p>Ingredient:</p> <input type="text" name="Ingredient_1" required size="45" class="barre" />  &ensp; <input type="text" name="Ingredient_2" required size="45" class="barre" />  &ensp; <input type="text" name="Ingredient_3" required size="45" class="barre" />  &ensp; <input type="text" name="Ingredient_4" required size="45" class="barre" />  &ensp;<input type="text" name="Ingredient_5" required size="45" class="barre" />  &ensp; <!--<button class="link">add</button>-->
+<p>Nom:</p> <br> <input type="text" name="Nom" class="barre" />
+
+<p>Ingredient: 
+
+<!-- ************************************** Choix nombre ingrédient(s) ****************************************** -->
+    
+<form action="" method="post">
+     <select name="Nbr" id="Nbr">
+           <option value="Nbr_1">1</option>
+           <option value="Nbr_2">2</option>
+           <option value="Nbr_3">3</option>
+           <option value="Nbr_4">4</option>
+           <option value="Nbr_5">5</option>
+    </select>
+<br> 
+<script>
+    document.getElementById(Nbr).value
+    
+</script>
+
+<!-- ************************************** /Choix nombre ingrédient(s)/ ****************************************** -->
 <br>
-<p>Url:<p> <input type="text" name="URL" required size="45" class="barre" /> 
+<p>Url:<p> <br> <input type="text" name="URL" required size="45" class="barre" /> 
 <br>
 <input type="submit" name="Bouton" class="bouton" >
 
